f22dc8f04a36d14e2d2d0e84eabc8e5a2cbea165
César García <cesar.garciamcs@outlook.com>
2024-07-25 19:25:16 -0600

    final commit

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,7 @@ import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
-import terser from 'gulp-terser'
 import sharp from 'sharp'
-
-export function js( done ) {
-    src('src/js/app.js')
-        .pipe(terser())
-        .pipe( dest('build/js') ) 
-
-    done()
-}
 
 export function css( done ) {
     src('src/scss/app.scss', {sourcemaps: true})
@@ -26,34 +17,6 @@ export function css( done ) {
         .pipe( dest('build/css', {sourcemaps: '.'}) )
 
     done()
-}
-
-export async function crop(done) {
-    const inputFolder = 'src/img/gallery/full'
-    const outputFolder = 'src/img/gallery/thumb';
-    const width = 250;
-    const height = 180;
-    if (!fs.existsSync(outputFolder)) {
-        fs.mkdirSync(outputFolder, { recursive: true })
-    }
-    const images = fs.readdirSync(inputFolder).filter(file => {
-        return /\.(jpg)$/i.test(path.extname(file));
-    });
-    try {
-        images.forEach(file => {
-            const inputFile = path.join(inputFolder, file)
-            const outputFile = path.join(outputFolder, file)
-            sharp(inputFile) 
-                .resize(width, height, {
-                    position: 'centre'
-                })
-                .toFile(outputFile)
-        });
-
-        done()
-    } catch (error) {
-        console.log(error)
-    }
 }
 
 export async function imagenes(done) {
@@ -87,8 +50,7 @@ function procesarImagenes(file, outputSubDir) {
 
 export function dev() {
     watch('src/scss/**/*.scss', css)
-    watch('src/js/**/*.js', js)
     watch('src/img/**/*.{png,jpg}', imagenes)
 }
 
-export default series( crop, js, css, imagenes, dev )
+export default series( css, imagenes, dev )
